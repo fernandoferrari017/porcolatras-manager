51eeefb040f598690e5d0d049b6b9285f9a32d88
fernandoferrari017 <fernandoferrari017@gmail.com>
2026-01-24 17:47:19 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { supabase } from './supabaseClient';
 import { 
